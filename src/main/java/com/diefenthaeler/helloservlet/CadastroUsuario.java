cd82b8a7ab9f66e9e597ec04677869df89178012
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-05-24 20:59:55 -0300

    Submit page

--- src/main/java/com/diefenthaeler/helloservlet/CadastroUsuario.java
+++ src/main/java/com/diefenthaeler/helloservlet/CadastroUsuario.java
@@ -4,6 +4,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CadastroUsuario", value = "/CadastroUsuario")
 public class CadastroUsuario extends HttpServlet {
@@ -14,6 +15,15 @@ public class CadastroUsuario extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String nome = request.getParameter("nome");
+        String sobrenome = request.getParameter("sobrenome");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + nome + " " + sobrenome + "</h1>");
+        out.println("<h1>" + request.getMethod() + "</h1>");
+        out.println("</body></html>");
 
     }
 }
